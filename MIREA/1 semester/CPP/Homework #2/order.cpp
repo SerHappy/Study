c425ca04112ef5a74c5df4966dd109fc3f5f0a6e
SerHappy <57107119+SerHappy@users.noreply.github.com>
2021-10-11 11:00:10 +0300

    Create order.cpp

---
+++ MIREA/1 semester/CPP/Homework #2/order.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int N;
+	cout << "Input N: ";
+	cin >> N;
+	cout << "The answer is: ";
+	for (int i = N; i <= N + 10; i++) {
+		cout << i << " ";
+	}
+}
