53039290860db61916858fd6a898ddb3b92c9bd4
SerHappy <57107119+SerHappy@users.noreply.github.com>
2021-10-11 11:11:24 +0300

    Create pseudo-random_number_generator.cpp

---
+++ MIREA/1 semester/CPP/Homework #4/pseudo-random_number_generator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main()
+{
+    int S0, m,i,c;
+    std::string choice;
+    std::cout << "1/2 Variant? : ";
+    std::cin >> choice;
+    if (choice == "1")
+    {
+        m = 37;
+        i = 3;
+        c = 64;
+    }
+    else
+    {
+        m = 25173;
+        i = 13849;
+        c = 65537;
+    }
+    
+    int Sn = 0;
+    for (int j = 1; j <= 10; j++)
+    {
+        S0 = (m * Sn + i) % c;
+        Sn = S0;
+        std::cout << S0 << '\n';
+    }
+	system("pause");
+	return 0;
+}
