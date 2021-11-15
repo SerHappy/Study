1a0a3900352c23f823ffbfe7044a4c44e05f9c4e
SerHappy <ppersikus@gmail.com>
2021-11-15 14:30:09 +0300

    Added balls.cpp

---
+++ MIREA/1 semester/CPP/balls.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s = "0123456789";
+    sort(s.begin(), s.end());
+    int sum = 0;
+    do
+    {
+        for (int i = 0; i < s.length(); ++i)
+        {
+            if (s[i] == '0' + i)
+            {
+                sum++;
+                break;
+            }
+        }
+    } while (next_permutation(s.begin(), s.end()));
+    cout << sum;
+}
